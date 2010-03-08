7961aa9c9eaa77b7218e5cd11cdb70015229f1d1
gsiener <siener@gmail.com>
2010-03-08 11:31:47 -0500

    added polling script

---
+++ poll.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'rmodbus'
+require 'bindata'
+
+id = ARGV[0].to_i
+reg = ARGV[1].to_i
+
+cl = ModBus::RTUClient.new('com3', 9600, id)
+data = cl.read_holding_registers(reg, 2)
+
+a = BinData::Int16be.new
+a.value = data[0]
+a.to_binary_s
+
+b = BinData::Int16be.new
+b.value = data[0]
+b.to_binary_s
+
+fl = BinData::FloatBe.read(a.to_binary_s+b.to_binary_s)
+
+puts fl.to_s
